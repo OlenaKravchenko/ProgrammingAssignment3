bf2e4cb67dbd69ab891839b51810a3c8dd821f69
Olena Kravchenko <31182006+OlenaKravchenko@users.noreply.github.com>
2018-02-23 07:03:18 +0200

    rankhospital.R
    Final version.

--- rankhospital.R
+++ rankhospital.R
@@ -33,23 +33,65 @@ rankhospital <- function(state, outcome, num) {
     pn_state <- which(pneumonia_all[, "state"] == state)
     pn_state_sear <- pneumonia_all[pn_state, ]
     colnames(pn_state_sear) <- c("hospital name", "state", "outcome")
-    min_pn <- pn_state_sear[order(pn_state_sear$outcome), ]
-    min_pn[1,1]
+    min_pn <- pn_state_sear[order(pn_state_sear$outcome, pn_state_sear$`hospital name`), ]
+    good_min_pn <- min_pn[complete.cases(min_pn), ]
+    if(num == "best") {
+      num_best <- 1
+      num <- num_best
+      num
+    }
+    if(num == "worst" & outcome == pn) {
+      num_worst_pn <- as.numeric(nrow(good_min_pn))
+      num <- num_worst_pn
+      num
+    }
+    if(outcome == pn & num > nrow(good_min_pn)) {
+      return("NA")
+    }
+    good_min_pn[num, 1]
   }
   else 
     if(outcome == hf) {
       hf_state <- which(heart_failure_all[, "state"] == state)
       hf_state_sear <- heart_failure_all[hf_state, ]
       colnames(hf_state_sear) <- c("hospital name", "state", "outcome")
-      min_hf <- hf_state_sear[order(hf_state_sear$outcome), ]
-      min_hf[1,1]
+      min_hf <- hf_state_sear[order(hf_state_sear$outcome, hf_state_sear$`hospital name`), ]
+      good_min_hf <- min_hf[complete.cases(min_hf), ]
+      if(num == "best") {
+        num_best <- 1
+        num <- num_best
+        num
+      }
+      if(num == "worst" & outcome == hf) {
+        num_worst_hf <- as.numeric(nrow(good_min_hf))
+        num <- num_worst_hf
+        num
+      }
+      if(outcome == hf & num > nrow(good_min_hf)) {
+        return("NA")
+      }
+      good_min_hf[num, 1]
     }
   else
     if(outcome == ha) {
       ha_state <- which(heart_attack_all[, "state"] == state)
       ha_state_sear <- heart_attack_all[ha_state, ]
       colnames(ha_state_sear) <- c("hospital name", "state", "outcome")
-      min_ha <- ha_state_sear[order(ha_state_sear$outcome), ]
-      min_ha[1,1]
+      min_ha <- ha_state_sear[order(ha_state_sear$outcome, ha_state_sear$`hospital name`), ]
+      good_min_ha <- min_ha[complete.cases(min_ha), ]
+      if(num == "best") {
+        num_best <- 1
+        num <- num_best
+        num
+      }
+      if(num == "worst" & outcome == ha) {
+        num_worst_ha <- as.numeric(nrow(good_min_ha))
+        num <- num_worst_ha
+        num
+      }
+      if(outcome == ha & num > nrow(good_min_ha)) {
+        return("NA")
+      }
+      good_min_ha[num,1]
     }
 }
